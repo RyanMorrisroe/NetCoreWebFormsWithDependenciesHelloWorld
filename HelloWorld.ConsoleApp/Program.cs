5440e04b5d0ee05c12e7e9b6f1d5bbf6802c832f
Ryan Morrisroe <6473229+RyanMorrisroe@users.noreply.github.com>
2019-09-25 00:11:25 -0500

    Added a console app for testing single file apps on other OSes

---
+++ HelloWorld.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HelloWorld.Library2;
+
+namespace HelloWorld.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Class2.SubtractThenAdd(1, 2, 3);
+            Console.WriteLine("I ran a basic function from a .NET standard library and the output was " + a.ToString());
+            Console.WriteLine("Press any key to exit out of the program.");
+            Console.ReadLine();
+        }
+    }
+}
